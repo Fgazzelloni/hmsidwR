6eca2e2d8e9cf37b846a8b68f2c1307950cf2c2d
fgazzelloni <docksbox@pm.me>
2024-07-12 18:14:30 +0200

    updating data

--- data-raw/deaths2019.R
+++ data-raw/deaths2019.R
@@ -16,7 +16,7 @@ deaths2019 <- data %>%
     age = gsub(" .*", "", age),
     age = gsub("1-4", "01-04", age),
     age = gsub("5-9", "05-09", age),
-    age = gsub("<1", "-01", age),
+    #age = gsub("<1", "-01", age),
     sex = tolower(sex),
     location = case_when(location == "United Kingdom" ~ "UK",
       location == "United States of America" ~ "US",
@@ -24,6 +24,14 @@ deaths2019 <- data %>%
     )
   )
 
+# Define the correct order of age groups
+age_levels <- c("<1", "01-04", "05-09", "10-14", "15-19", "20-24", "25-29", "30-34",
+                "35-39", "40-44", "45-49", "50-54", "55-59", "60-64",
+                "65-69", "70-74", "75-79", "80-84", "85+")
+
+# Convert the Age column to a factor with the specified order
+deaths2019$age <- factor(deaths2019$age, levels = age_levels, ordered = TRUE)
+deaths2019 <- deaths2019[order(deaths2019$age), ]
 
 usethis::use_data(deaths2019, overwrite = TRUE)
 devtools::document()
--- data-raw/gho_lifetables.R
+++ data-raw/gho_lifetables.R
@@ -20,13 +20,20 @@ gho_lifetables <- data %>%
     age = gsub(" .*", "", age),
     age = gsub("1-4", "01-04", age),
     age = gsub("5-9", "05-09", age),
-    age = gsub("<1", "-01", age),
+    # age = gsub("<1", "-01", age),
     indicator = gsub(" .*", "", indicator),
     sex = ifelse(sex == "Both sexes", "both", sex),
     sex = tolower(sex)
   )
 
+# Define the correct order of age groups
+age_levels <- c("<1", "01-04", "05-09", "10-14", "15-19", "20-24", "25-29", "30-34",
+                "35-39", "40-44", "45-49", "50-54", "55-59", "60-64",
+                "65-69", "70-74", "75-79", "80-84", "85+")
 
+# Convert the Age column to a factor with the specified order
+gho_lifetables$age <- factor(gho_lifetables$age, levels = age_levels, ordered = TRUE)
+gho_lifetables <- gho_lifetables[order(gho_lifetables$age), ]
 
 usethis::use_data(gho_lifetables, overwrite = TRUE)
 devtools::document()
