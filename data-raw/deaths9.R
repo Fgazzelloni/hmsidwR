fc5878eafb909f36dd70f7441e4e4b41be00d64c
fgazzelloni <docksbox@pm.me>
2024-07-12 17:14:00 +0200

    updating order

--- data-raw/deaths9.R
+++ data-raw/deaths9.R
@@ -20,6 +20,15 @@ deaths <- data %>%
       location == "United States of America" ~ "US",
       .default = location))
 
+# Define the correct order of age groups
+age_levels <- c("<5", "05-09", "10-14", "15-19", "20-24", "25-29", "30-34",
+                "35-39", "40-44", "45-49", "50-54", "55-59", "60-64",
+                "65-69", "70-74", "75-79", "80-84", "85+")
+
+# Convert the Age column to a factor with the specified order
+deaths$age <- factor(deaths$age, levels = age_levels, ordered = TRUE)
+deaths <- deaths[order(deaths$age), ]
+
 location <- deaths$location |> unique()
 iso2 <- countrycode(location, 'country.name', 'iso2c')
 
